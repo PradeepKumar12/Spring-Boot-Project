d343660cea569ea39532fc2e52181b8727e3130a
Quarks <Quarks@DESKTOP-LE8IBLJ>
2021-02-25 16:36:04 +0530

    commit

--- Boot_JPA_Example/src/main/java/com/jpa/BootJpaExampleApplication.java
+++ Boot_JPA_Example/src/main/java/com/jpa/BootJpaExampleApplication.java
@@ -1,5 +1,8 @@
 package com.jpa;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -14,14 +17,31 @@ public class BootJpaExampleApplication {
 		ApplicationContext applicationContext= SpringApplication.run(BootJpaExampleApplication.class, args);
 		UserRepository userRepository=applicationContext.getBean(UserRepository.class);
 		
-		User user=new User();
-		user.setId(1);
-		user.setName("Ram");
-		user.setCity("noida");
+		
+//		User user=new User();
+//		user.setId(1);
+//		user.setName("Ram");
+//		user.setCity("noida");
+//// Saving Single User		
+//		User result=userRepository.save(user);
+//		System.out.println(result);
+//		
+		
+		User user1=new User();
+		user1.setName("Mohan");
+		user1.setCity("NCR");
 	
-		User result=userRepository.save(user);
-		System.out.println(result);
+		User user2=new User();
+		user2.setName("Rohan");
+		user2.setCity("RBL");
+		
+		List<User> list=List.of(user1,user2);
+		Iterable<User> it =userRepository.saveAll(list);
+		it.forEach(result->{
+			System.out.println(result);
+		});
 	
+		
 	}
 
 }
